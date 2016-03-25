aece94e1d056686dc9e8fa32aed0ff653ee39cd3
Frazier Mork <fraziermork@gmail.com>
2016-03-24 22:40:34 -0700

    got email working from a form

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 let express       = require('express');
 let app           = express();
 let emailHandler  = require(__dirname + '/lib/email.js');
-
+let bodyParser    = require('body-parser');
 
 app.get('*', (req, res) => {
   console.log('request made to get index page');
   res.sendFile('index.html', {root: '.'});
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.post('/email', emailHandler);
 
 app.listen(3000, () => {
   console.log('listening on 3000');
